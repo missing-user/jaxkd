4cf609dddc9f2cbbd35ba7f930b46e3d30193d35
Benjamin Dodge <bendodge@stanford.edu>
2025-05-13 15:52:47 -0700

    fix test script to ensure execution during timing

--- test.py
+++ test.py
@@ -15,9 +15,10 @@ for i in range(3):
     # points = jax.random.uniform(subkey, shape=(n_points, 2))
     points = jax.random.normal(subkey, shape=(n_points, 2))
     tree = jk.make_kd_tree(points)
+    tree[0].block_until_ready()
     print(f'Built kd-tree in {time.time() - start:.3f} seconds')
 
-for i in range(3):
     start = time.time()
     neighbors, distances = jax.vmap(lambda query: jk.query_neighbors(query, points, *tree, k=4))(points)
+    neighbors.block_until_ready()
     print(f'Queried all points in {time.time() - start:.3f} seconds')
